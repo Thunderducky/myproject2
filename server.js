b2ab71d67d43c9506be1477f6ad12935080f8221
Eric Scott <ericscott@Erics-MacBook-Pro.local>
2019-07-15 10:53:40 -0500

    the smallest of changes

--- server.js
+++ server.js
@@ -37,7 +37,7 @@ var syncOptions = { force: false };
 if (process.env.NODE_ENV === "test") {
   syncOptions.force = true;
 }
-
+  
 // Starting the server, syncing our models ------------------------------------/
 db.sequelize.sync(syncOptions).then(function() {
   app.listen(PORT, function() {
